ebeb1a15ef471047bad58dbc568e2e1c1cef4f6d
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-09-14 13:28:24 +0500

    added the logo part to superadmin side

--- components/superadminHeader.tsx
+++ components/superadminHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from "@/public/logo.jpg"
 
 interface HeaderProps {
   userName: string;
@@ -7,12 +8,14 @@ interface HeaderProps {
 
 const AdminHeader: React.FC<HeaderProps> = ({ userName, userEmail }) => {
   return (
-    <div className="w-full bg-white shadow-md p-6">
-      <div className="flex flex-col items-end text-gray-600 space-y-2">
-        <span className="font-bold">{userName}</span>
-        <span>{userEmail}</span>
-      </div>
+    <div className="w-full bg-white shadow-md p-6 h-16 flex justify-between items-center">
+    <img src={Image.src} alt="Logo" className="mr-4" width={60} height={60}/>
+    <div className="flex flex-col items-end text-gray-600 space-y-2 flex-grow">
+      <span className="font-bold">{userName}</span>
+      <span>{userEmail}</span>
     </div>
+  </div>
+  
   );
 };
 
