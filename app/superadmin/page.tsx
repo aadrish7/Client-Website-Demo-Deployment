6048594f397fa0a3535602dcc4f47c0774972319
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-09-23 18:36:33 +0500

    resolved the build error

--- app/superadmin/page.tsx
+++ app/superadmin/page.tsx
@@ -8,11 +8,170 @@ import outputs from '@/amplify_outputs.json';
 import Header from '@/components/superadminHeader'; 
 import Sidebar from '@/components/superadminSidebar';
 import Table from '@/components/table';   
-import CreatCompanyComponent from './createcompany/page' 
+
 
 Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
+
+interface CompanyForm {
+  companyName: string;
+  adminEmail: string;
+  adminFirstName: string;
+  adminLastName: string;
+  adminJobTitle: string;
+}
+
+interface CreateCompanyPageProps {
+  onClose: () => void; // Define onClose prop type
+}
+
+const CreateCompanyPage: React.FC<CreateCompanyPageProps> = ({ onClose }) => {
+  const [formData, setFormData] = useState<CompanyForm>({
+    companyName: '',
+    adminEmail: '',
+    adminFirstName: '',
+    adminLastName: '',
+    adminJobTitle: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [successMessage, setSuccessMessage] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { companyName, adminEmail, adminFirstName, adminLastName, adminJobTitle } = formData;
+    if (!companyName || !adminEmail || !adminFirstName || !adminLastName || !adminJobTitle) {
+      setErrorMessage('All fields are required.');
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const { data: company } = await client.models.Company.create({
+        companyName,
+        adminEmail,
+        adminFirstName,
+        adminLastName,
+        adminJobTitle,
+      });
+
+      setSuccessMessage('Company created successfully!');
+      setErrorMessage('');
+      clearForm();
+      router.push('/superadmin');
+      onClose(); // Close the modal after successful creation
+    } catch (error) {
+      setErrorMessage('An error occurred while creating the company.');
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const clearForm = () => {
+    setFormData({
+      companyName: '',
+      adminEmail: '',
+      adminFirstName: '',
+      adminLastName: '',
+      adminJobTitle: '',
+    });
+  };
+
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-10">
+      <div className="bg-white p-6 rounded-md shadow-lg w-full max-w-md">
+        <h2 className="text-lg font-semibold mb-7">Create a New Company</h2>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="mb-6 mt-4">
+            <label className="text-sm block font-medium mb-2">Company Name</label>
+            <input
+              type="text"
+              name="companyName"
+              value={formData.companyName}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded p-2 w-full bg-gray-100 text-sm text-black"
+              required
+            />
+          </div>
+          <div className="mb-6 mt-4">
+            <label className="text-sm block font-medium mb-2">Admin Email</label>
+            <input
+              type="email"
+              name="adminEmail"
+              value={formData.adminEmail}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded p-2 w-full bg-gray-100 text-sm text-black"
+              required
+            />
+          </div>
+          <div className="mb-6 mt-4">
+            <label className="text-sm block font-medium mb-2">Admin First Name</label>
+            <input
+              type="text"
+              name="adminFirstName"
+              value={formData.adminFirstName}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded p-2 w-full bg-gray-100 text-sm text-black"
+              required
+            />
+          </div>
+          <div className="mb-6 mt-4">
+            <label className="text-sm block font-medium mb-2">Admin Last Name</label>
+            <input
+              type="text"
+              name="adminLastName"
+              value={formData.adminLastName}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded p-2 w-full bg-gray-100 text-sm text-black"
+              required
+            />
+          </div>
+          <div className="mb-6 mt-4">
+            <label className="text-sm block font-medium mb-2">Admin Job Title</label>
+            <input
+              type="text"
+              name="adminJobTitle"
+              value={formData.adminJobTitle}
+              onChange={handleInputChange}
+              className="border border-gray-300 rounded p-2 w-full bg-gray-100 text-sm text-black"
+              required
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              onClick={onClose} // Use onClose here
+              className="bg-gray-500 text-white px-4 py-2 rounded-md mr-2"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-600 text-white px-4 py-2 rounded-md"
+              disabled={loading}
+            >
+              {loading ? 'Creating Company...' : 'Create Company'}
+            </button>
+          </div>
+        </form>
+        {errorMessage && <p className="text-red-500 mt-4">{errorMessage}</p>}
+        {successMessage && <p className="text-black mt-4">{successMessage}</p>}
+      </div>
+    </div>
+  );
+};
 
 const SuperAdminMainPage: React.FC = () => {
   const [popUp, setPopUp] = useState<boolean>(false)
@@ -107,7 +266,7 @@ const SuperAdminMainPage: React.FC = () => {
           </div>
         </div>
       </div>
-      {popUp && (<CreatCompanyComponent onClose={handleClosePopUp}/>)}
+      {popUp && (<CreateCompanyPage onClose={handleClosePopUp}/>)}
     </div>
   );
 };
--- app/superadmin/snippets/page.tsx
+++ app/superadmin/snippets/page.tsx
@@ -9,12 +9,126 @@ import Header from '@/components/superadminHeader';
 import Sidebar from '@/components/superadminSidebar';
 import Table from '@/components/table';    
 import Papa from 'papaparse';
-import ManualCSVPopup from "./createsnippet/page"
 
 
 Amplify.configure(outputs);
 const client = generateClient<Schema>();
+interface CreateTextSnippetProps {
+  onClose: () => void;
+}
 
+const CreateTextSnippet: React.FC<CreateTextSnippetProps> = ({ onClose }) => {
+  const [factor, setFactor] = useState<string>(''); 
+  const [score, setScore] = useState<string>(''); 
+  const [snippetText, setSnippetText] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false); // Added loading state
+
+  const router = useRouter();
+
+  const factors = ['Advocacy', 'Psychological Safety', 'Flexibility', 'Growth Satisfaction', 'Purpose'];
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setErrorMessage(null);
+    setSuccessMessage(null);
+    setLoading(true); // Start loading when form is submitted
+
+    try {
+      if (!factor || !score || !snippetText) {
+        setErrorMessage('All fields are required.');
+        setLoading(false); // Stop loading on error
+        return;
+      }
+
+      await client.models.TextSnippet.create({
+        factor,
+        score: Number(score),
+        snippetText,
+      });
+
+      setFactor('');
+      setScore('');
+      setSnippetText('');
+      setSuccessMessage('Text Snippet created successfully!');
+      
+      setLoading(false); // Stop loading after success
+      onClose(); // Call onClose after successful creation
+    } catch (error) {
+      console.error('Failed to create text snippet', error);
+      setErrorMessage('Failed to create text snippet. Please try again.');
+      setLoading(false); // Stop loading on error
+    }
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-10">
+      <div className="w-full max-w-md p-6 bg-white rounded-md shadow-lg">
+        <h1 className="text-lg font-semibold mb-7">Create Text Snippet</h1>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium mb-2">Factor</label>
+            <select
+              value={factor}
+              onChange={(e) => setFactor(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded bg-gray-100 text-sm"
+              required
+            >
+              <option value="" disabled>Select a factor</option>
+              {factors.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-2">Score</label>
+            <input
+              type="text"
+              value={score}
+              onChange={(e) => setScore(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded bg-gray-100 text-sm"
+              placeholder="Enter score"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-2">Snippet Text</label>
+            <textarea
+              value={snippetText}
+              onChange={(e) => setSnippetText(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded bg-gray-100 text-sm"
+              placeholder="Enter snippet text"
+              rows={4}
+              required
+            ></textarea>
+          </div>
+
+          <div className="flex justify-center">
+          <button onClick={onClose} className="bg-gray-500 text-white px-4 py-2 rounded-md mr-2">
+            Cancel
+          </button>
+            <button
+              type="submit"
+              className="bg-blue-600 text-white px-4 py-2 rounded-md"
+              disabled={loading} // Disable button while loading
+            >
+              {loading ? 'Creating...' : 'Create Text Snippet'}
+            </button>
+            
+          </div>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          {successMessage && <p className="text-black">{successMessage}</p>}
+        </form>
+      </div>
+    </div>
+  );
+};
 const SuperAdminMainPage: React.FC = () => {
   const [tableHeaders, setTableHeaders] = useState<string[]>([]);
   const [tableData, setTableData] = useState<Record<string, string>[]>([]);
@@ -189,7 +303,7 @@ const SuperAdminMainPage: React.FC = () => {
       </div>
       
       )}
-      {showManualCreationPopup && (<ManualCSVPopup onClose={onClose}/>)}
+      {showManualCreationPopup && (<CreateTextSnippet onClose={onClose}/>)}
     </div>
   );
 };
