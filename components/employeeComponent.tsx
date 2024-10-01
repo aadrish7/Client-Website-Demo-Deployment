6e8f7e012a6f6eec254f61f906e2d0bc6bcfb80a
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-10-01 23:43:19 +0500

    pushed the factor importance page to the end

--- components/employeeComponent.tsx
+++ components/employeeComponent.tsx
@@ -70,7 +70,7 @@ const QuestionsComponent: React.FC = () => {
   const steps = ["Create Account", "Assessment", "Survey Results"];
 
   async function handleFinish() {
-    setIsViewingResults(() => true);
+    console.log("in handle finish")
     const updatedSelections: Record<
       string,
       { questionId: string | undefined; selection: string }[]
@@ -111,11 +111,10 @@ const QuestionsComponent: React.FC = () => {
             score: value || 0,
           });
       }
-      setIsFinished(() => false);
-      setIsViewingResults(() => false);
+
+      setIsFinished(() => true);
       // setCurrentQuestionNumber((prev) => prev + 1);
       setCurrentStep((currentStep) => currentStep + 1);
-      setViewSurveyResults(() => true);
     } catch (error) {
       console.error("Error saving data:", error);
     }
@@ -135,12 +134,14 @@ const QuestionsComponent: React.FC = () => {
     });
   };
 
-  const handleFactorImportanceButton = () => {
+  const handleFactorImportanceButton = async() => {
     if (Object.values(selectedValues).includes(null)) {
       alert("Please rate all categories before proceeding");
       return;
     }
+    await handleFinish();
     setFactorImportanceBool(() => false);
+    setIsFinished(() => true);
   };
 
   const getQuestions = async () => {
@@ -277,7 +278,7 @@ const QuestionsComponent: React.FC = () => {
 
       const validQuestions = questionList.filter((q) => q !== null);
 
-      setTotalQuestions(() => validQuestions.length);
+      setTotalQuestions(() => validQuestions.length+1);
 
       const questionsByFactor: QuestionsByFactor = {};
       validQuestions.forEach((question: any) => {
@@ -375,7 +376,6 @@ const QuestionsComponent: React.FC = () => {
 
   const handleFirstAttempt = () => {
     setFirstAttempt(() => false);
-    setFactorImportanceBool(() => true);
   };
 
   const handleNextQuestion = () => {
@@ -383,7 +383,6 @@ const QuestionsComponent: React.FC = () => {
     if (!selectedOption) return;
 
     const factorQuestions = questionsByFactor[currentFactor];
-
     // Save the selected option before moving to the next question
     if (selectedOption !== null) {
       setUserSelections((prevSelections) => {
@@ -423,7 +422,8 @@ const QuestionsComponent: React.FC = () => {
         setSelectedOption(existingSelection); // Set the previously selected option or null
         setCurrentQuestionNumber((prev) => prev + 1);
       } else {
-        setIsFinished(true);
+        console.warn("No more questions available.");
+        setFactorImportanceBool(() => true);
         setCurrentQuestionNumber((prev) => prev + 1);
         setCurrentStep((currentStep) => currentStep + 1);
       }
@@ -536,7 +536,7 @@ const QuestionsComponent: React.FC = () => {
             className={`bg-blue-600 text-white rounded px-2 py-2 ${
               isViewingResults ? "opacity-50 cursor-not-allowed" : ""
             }`}
-            onClick={handleFinish}
+            onClick={() => setViewSurveyResults(() => true)}
             disabled={isViewingResults}
           >
             {isViewingResults ? "Viewing Reports..." : "View Report"}
@@ -607,6 +607,8 @@ const QuestionsComponent: React.FC = () => {
           onSelectionChange={handleSelection}
           factorImportanceBool={factorImportanceBool}
           onButtonClick={handleFactorImportanceButton}
+          currentQuestionNumber={currentQuestionNumber}
+          totalQuestions={totalQuestions}
         />
       </>
     );
@@ -674,7 +676,7 @@ const QuestionsComponent: React.FC = () => {
             currentQuestionIndex === currentQuestions.length - 1 &&
             Object.keys(questionsByFactor).indexOf(currentFactor) ===
               Object.keys(questionsByFactor).length - 1
-              ? () => setIsFinished(true)
+              ? handleNextQuestion
               : handleNextQuestion
           }
           disabled={selectedOption === null}
