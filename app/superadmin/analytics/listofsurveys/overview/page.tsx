147c7b54ed102ae3ef137cd622e48ee6106fb826
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-10-03 17:12:05 +0500

    worked on analyics side of superadmin

---
+++ app/superadmin/analytics/listofsurveys/overview/page.tsx
@@ -0,0 +1,347 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import Header from "@/components/superadminHeader";
+import Sidebar from "@/components/superadminSidebar";
+import DropdownButton from "@/components/dropDownButton";
+import dynamic from "next/dynamic";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import AdminEmployeeComponent from "@/components/adminEmployeeComponent";
+
+const PieChart = dynamic(() => import("@/components/adminPieChart"), {
+  ssr: false,
+  loading: () => <div className="text-center py-4">Loading Graph...</div>,
+});
+
+const BarChart = dynamic(() => import("@/components/barChartEmployee"), {
+  ssr: false,
+  loading: () => <div className="text-center py-4">Loading Graph...</div>,
+});
+
+Amplify.configure(outputs);
+const client = generateClient<Schema>();
+
+const OverviewPage: React.FC = () => {
+  const searchParams = useSearchParams();
+  const [surveyName, setSurveyName] = useState<string>("");
+  const [companyName, setComoanyName] = useState<string>("");
+  const [percentageFactorImportance, setPercentageFactorImportance] = useState<{
+    [key: string]: number;
+  }>({});
+  const [averageScores, setAverageScores] = useState<{ [key: string]: number }>(
+    {}
+  );
+  //array of strings
+  const [matchingSnippets, setMatchingSnippets] = useState<any>([]);
+
+  const isScoreInRange = (score: number, range: Number): boolean => {
+    const rangeValue = range.valueOf();
+    const min = rangeValue - 0.49;
+    const max = rangeValue + 0.5;
+    return score >= min && score <= max;
+  };
+
+
+
+  const preparingDataForPercentagePieChart = (
+    factorImportanceResponses: { factor: string; score: number }[]
+  ) => {
+    // Step 1: Filter responses to include only those with a score of 5
+    const factorImportanceResponsesFiltered = factorImportanceResponses.filter(
+      (response) => response.score === 5
+    );
+    console.log("5factorImportanceResponsesFiltered", factorImportanceResponsesFiltered);
+
+    // Step 2: Count occurrences of each factor
+    const factorImportanceCount = factorImportanceResponsesFiltered.reduce(
+      (acc, response) => {
+        if (!acc[response.factor]) {
+          acc[response.factor] = 0;
+        }
+        acc[response.factor] += 1;
+        return acc;
+      },
+      {} as { [key: string]: number }
+    );
+
+    // Step 3: Calculate the percentage of each factor
+    const totalResponses = factorImportanceResponsesFiltered.length;
+    const factorImportancePercentage = Object.keys(
+      factorImportanceCount
+    ).reduce((acc, factor) => {
+      acc[factor] = totalResponses
+        ? parseFloat(
+            ((factorImportanceCount[factor] / totalResponses) * 100).toFixed(2)
+          )
+        : 0; // Handle division by zero if totalResponses is 0
+      return acc;
+    }, {} as { [key: string]: number });
+
+    // Step 4: Update the state with the calculated percentages
+    setPercentageFactorImportance(factorImportancePercentage);
+  };
+
+
+  const fetchData = async () => {
+    const idOfSurvey = searchParams.get("surveyId") || "";
+
+    const { data: surveys } = await client.models.Survey.list({
+      filter: {
+        id: {
+          eq: idOfSurvey,
+        },
+      },
+    });
+    if (surveys.length === 0) {
+      console.error("No surveys found for company:");
+      return;
+    }
+    const survey = surveys[0];
+    setSurveyName(survey.surveyName);
+   if (!survey.companyId) {
+      console.error("No company found for survey:", survey);
+      return;
+    } 
+    const { data: companies } = await client.models.Company.list({
+      filter: {
+        id: {
+          eq: survey.companyId,
+        },
+      },
+    });
+    if (companies.length === 0) {
+      console.error("No companies found for survey:", survey);
+      return;
+    }
+    const company = companies[0];
+    if (!company.companyName) {
+      console.error("No company name found for company:", company);
+      return;
+    }
+    setComoanyName(company.companyName);
+
+    const { data: factorImportanceResponses } =
+    await client.models.FactorImportance.list({
+      filter: {
+        surveyId: {
+          eq: survey.id,
+        },
+      },
+    });
+
+   
+
+    preparingDataForPercentagePieChart(factorImportanceResponses)
+
+    const {data : averageSurveyResponses} =  await client.models.AverageSurveyResults.list({
+      filter : {
+        surveyId: {
+          eq: survey.id,
+        },
+      }
+    })
+    if (averageSurveyResponses.length == 0){
+      console.error("No Average Surveys found")
+    }
+    const allResponses: any[] = [];
+    averageSurveyResponses.forEach((response) => {
+      if (typeof response.averageScorejson === "string") {
+        const surveyResponse = JSON.parse(response.averageScorejson);
+        allResponses.push(surveyResponse);
+      } else {
+        console.error(
+          "Invalid type for averageScorejson:",
+          typeof response.averageScorejson
+        );
+      }
+    });
+    const finalAverage = averageSurveyResponses[0];
+    const totalScores: { [key: string]: { total: number; count: number } } = {};
+    allResponses.forEach((response) => {
+      Object.keys(response).forEach((factor) => {
+        if (!totalScores[factor]) {
+          totalScores[factor] = { total: 0, count: 0 };
+        }
+        totalScores[factor].total += response[factor];
+        totalScores[factor].count += 1;
+      });
+    });
+
+    const avgScores = Object.keys(totalScores).reduce((acc, factor) => {
+      acc[factor] = totalScores[factor].total / totalScores[factor].count;
+      return acc;
+    }, {} as { [key: string]: number });
+    console.log("average", avgScores)
+
+    setAverageScores(avgScores)
+  };
+
+  useEffect(() => {
+    if (searchParams.has("surveyId")) {
+      fetchData();
+
+    }
+  });
+  const getSnippets = async () => {
+    const { data: overviewSnippets } = await client.models.OverviewTextSnippet.list({});
+
+    if (overviewSnippets.length === 0) {
+      console.error("No snippets found for company:");
+      return;
+    }
+    return overviewSnippets;
+  }
+  useEffect(() => {
+    const findMatchingSnippets = async () => {
+      if (Object.keys(averageScores).length > 0) {
+        // Convert the averageScores object to an array of entries and sort it by the score value
+        const sortedScores = Object.entries(averageScores).sort(([, scoreA], [, scoreB]) => scoreB - scoreA);
+
+        console.log("sortedScores", sortedScores);
+    
+        // Fetch the snippets
+        const snippets = await getSnippets();
+        if (!snippets) {
+          console.error("No snippets found for company:");
+          return;
+        }
+    
+        // Filter snippets based on sortedScores
+        const matchedSnippets = snippets.filter((snippet: any) => {
+          // Find the factorScore from the sorted averageScores
+          const factorScoreEntry = sortedScores.find(([factor]) => factor === snippet.factor);
+          const factorScore = factorScoreEntry ? factorScoreEntry[1] : null;
+          return factorScore && isScoreInRange(factorScore, snippet.score);
+        });
+    
+        // Reverse the matchedSnippets and set them
+        setMatchingSnippets(matchedSnippets);
+      }
+    };
+    
+    findMatchingSnippets();
+    
+  }, [averageScores]);
+
+  const navItems = [
+    {
+      label: "📦 Collections",
+      active: false,
+      subItems: [
+        {
+          label: "📋 Question Bank",
+          active: false,
+          href: "/superadmin/collections/questionbank",
+        },
+        {
+          label: "📦 Collection",
+          active: false,
+          href: "/superadmin/collections/collection",
+        },
+      ],
+    },
+    {
+      label: "📦 Snippets",
+      active: false,
+      subItems: [
+        {
+          label: "📋 Snippet Bank",
+          active: false,
+          href: "/superadmin/snippets",
+        },
+        {
+          label: "📦 Snippet Set",
+          active: false,
+          href: "/superadmin/snippets/snippetset",
+        },
+      ],
+    },
+    {
+      label: "📦 Overview Snippets",
+      active: false,
+      subItems: [
+        {
+          label: "📋 Snippet Bank",
+          active: false,
+          href: "/superadmin/overviewsnippets",
+        },
+        {
+          label: "📦 Snippet Set",
+          active: false,
+          href: "/superadmin/overviewsnippets/overviewsnippetset",
+        },
+      ],
+    },
+
+    { label: "🏢 Company", active: false, href: "/superadmin" },
+    { label: "📊 Analytics", active: true, href: "/superadmin/analytics" },
+  ].filter((item) => item !== undefined);
+
+  return (
+    <div className="h-screen flex flex-col">
+      <Header userName="Neil Sims" userEmail="neilsimsemail@example.com" />
+      <div className="flex flex-1">
+        <Sidebar navItems={navItems} />
+        <div className="w-4/5 p-3 bg-gray-50 flex flex-col">
+          {/* Section with charts and summary paragraph */}
+          <div className="bg-white p-6 rounded-lg shadow-lg mb-6">
+            {surveyName.length > 0 ?(<h1 className="text-[20px] font-bold mb-6">
+              For <span className="text-blue-500">{companyName}</span>! Here is the
+              {" "}<span className="text-blue-500">{surveyName} </span> survey summary.
+            </h1>) : (<h1 className="text-[20px] font-bold mb-6"></h1>)}
+
+            <div className="flex mb-6">
+              {/* Pie Chart Section */}
+              <div className="w-full md:w-1/2 pr-4 border-b border-r">
+                <h2 className="text-sm font-semibold mb-2 flex items-center">
+                  % of employees rated each factor as the most important
+                  <span className="ml-1 text-gray-400">ⓘ</span>
+                </h2>
+                <div className="w-full h-96 pl-5 pt-6">
+                  <PieChart data={percentageFactorImportance} />
+                </div>
+              </div>
+
+              {/* Bar Chart Section */}
+              <div className="w-full md:w-1/2 pl-4 border-b">
+                <h2 className="text-sm font-semibold mb-2 flex items-center">
+                  Average score for each factor
+                  <span className="ml-1 text-gray-400">ⓘ</span>
+                </h2>
+                <div className="w-full h-96">
+                  <BarChart data={averageScores} />
+                </div>
+              </div>
+            </div>
+
+            {/* Paragraph Summary */}
+            <div className="text-gray-600 text-sm">
+              <p>
+              {matchingSnippets.map((snippet: any, index: any) => (
+                <span key={index}>{snippet.snippetText} {" "}</span>
+              ))}
+              </p>
+            </div>
+          </div>
+
+          {/* Section with AdminEmployeeComponent */}
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <AdminEmployeeComponent />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function () {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OverviewPage />
+    </Suspense>
+  );
+}
---
+++ app/superadmin/analytics/page.tsx
@@ -0,0 +1,147 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+import { useRouter } from "next/navigation";
+import outputs from "@/amplify_outputs.json";
+import Header from "@/components/superadminHeader";
+import Sidebar from "@/components/superadminSidebar";
+import Table from "@/components/table";
+
+Amplify.configure(outputs);
+const client = generateClient<Schema>();
+
+const Analytics = () => {
+
+    const [popUp, setPopUp] = useState<boolean>(false);
+    const [tableHeaders, setTableHeaders] = useState<string[]>([]);
+    const [tableData, setTableData] = useState<Record<string, string>[]>([]);
+    const [companies, setCompanies] = useState<any>([]);
+    const router = useRouter();
+  
+    const handleIdClick = async (id: string) => {
+
+
+      for (const company of companies) {
+        if (company.companyName === id) {
+            id  =  company.id;
+            break;
+        }
+        }
+      router.push(`analytics/listofsurveys?companyId=${id}`);
+    };
+  
+    const fetchCompanies = async () => {
+      try {
+        const { data: companyList } = await client.models.Company.list({});
+        setCompanies(()=>companyList);
+        setTableHeaders(() => ["company name", "admin email"]);
+        setTableData(
+          companyList.map((collection: any) => ({
+            "company name" : collection.companyName,
+            "admin email": collection.adminEmail,
+          }))
+        );
+      } catch (error) {
+        console.error("Failed to fetch collections");
+        console.error("Error:", error);
+      }
+    };
+  
+    useEffect(() => {
+      fetchCompanies();
+    }, []);
+  
+    const navItems = [
+      {
+        label: "📦 Collections",
+        active: false,
+        subItems: [
+          {
+            label: "📋 Question Bank",
+            active: false,
+            href: "/superadmin/collections/questionbank",
+          },
+          {
+            label: "📦 Collection",
+            active: false,
+            href: "/superadmin/collections/collection",
+          },
+        ],
+      },
+      {
+        label: "📦 Snippets",
+        active: false,
+        subItems: [
+          {
+            label: "📋 Snippet Bank",
+            active: false,
+            href: "/superadmin/snippets",
+          },
+          {
+            label: "📦 Snippet Set",
+            active: false,
+            href: "/superadmin/snippets/snippetset",
+          },
+        ],
+      },
+      {
+        label: "📦 Overview Snippets",
+        active: false,
+        subItems: [
+          {
+            label: "📋 Snippet Bank",
+            active: false,
+            href: "/superadmin/overviewsnippets",
+          },
+          {
+            label: "📦 Snippet Set",
+            active: false,
+            href: "/superadmin/overviewsnippets/overviewsnippetset",
+          },
+        ],
+      },
+  
+      { label: "🏢 Company", active: false, href: "/superadmin" },
+      { label: "📊 Analytics", active: true, href: "/superadmin/analytics" },
+    ].filter((item) => item !== undefined);
+  
+    const handleClosePopUp = () => {
+      setPopUp(false);
+      fetchCompanies();
+    };
+  
+    return (
+      <div className="h-screen flex flex-col">
+        <Header userName="Neil Sims" userEmail="neilsimsemail@example.com" />
+        <div className="flex flex-1">
+          <Sidebar navItems={navItems} />
+          <div className="w-4/5 p-8">
+            <h1 className="text-2xl font-semibold mb-6">Companies</h1>
+            <div className="border p-4">
+              <div className="flex items-center mb-4 justify-end">
+                <div className="flex space-x-4">
+                </div>
+              </div>
+  
+              {/* Generalized Table */}
+              {tableData && tableHeaders ? (
+                <Table
+                  headers={tableHeaders}
+                  data={tableData}
+                  handleClick={handleIdClick}
+                  underlineColumn="company name"
+                />
+              ) : (
+                <p>Loading Table...</p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+
+export default Analytics;
