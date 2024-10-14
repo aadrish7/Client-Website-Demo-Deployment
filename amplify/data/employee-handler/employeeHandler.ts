f3b715f1889b2c41756078885ff6cb3f43d06251
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-10-14 22:02:04 +0500

    resolved build error

--- amplify/data/employee-handler/employeeHandler.ts
+++ amplify/data/employee-handler/employeeHandler.ts
@@ -1,119 +1,119 @@
-import type { Schema } from '../resource'
-import { generateClient } from "aws-amplify/data";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+// import type { Schema } from '../resource'
+// import { generateClient } from "aws-amplify/data";
+// import { Amplify } from "aws-amplify";
+// import outputs from "@/amplify_outputs.json";
 
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
-export const handler: Schema["bulkCreateEmployees"]["functionHandler"] = async (event, context) => {
+// export const handler: Schema["bulkCreateEmployees"]["functionHandler"] = async (event, context) => {
     
-    let employeeArray = event.arguments.employeesArray || [];
+//     let employeeArray = event.arguments.employeesArray || [];
 
 
 
-    for (let i = 0; i < employeeArray.length; i++) {
-        const user: any = colonStringToObject(employeeArray[i] ? String(employeeArray[i]) : '')
-        try {
-            await client.models.User.create({
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                dob: user.dob,
-                hireDate: user.hireDate,
-                gender: user.gender,
-                ethnicity: user.ethnicity,
-                manager: user.manager,
-                location: user.location,
-                veteranStatus: user.veteranStatus,
-                disabilityStatus: user.disabilityStatus,
-                jobLevel: user.jobLevel,
-                department: user.department,
-                companyId: user.companyId,
-                surveyId: user.surveyId, // Store surveyId
-                role: 'employee',
-              });
-        } catch (error) {
-          throw new Error(`${error}`);
-        }
+//     for (let i = 0; i < employeeArray.length; i++) {
+//         const user: any = colonStringToObject(employeeArray[i] ? String(employeeArray[i]) : '')
+//         try {
+//             await client.models.User.create({
+//                 firstName: user.firstName,
+//                 lastName: user.lastName,
+//                 email: user.email,
+//                 dob: user.dob,
+//                 hireDate: user.hireDate,
+//                 gender: user.gender,
+//                 ethnicity: user.ethnicity,
+//                 manager: user.manager,
+//                 location: user.location,
+//                 veteranStatus: user.veteranStatus,
+//                 disabilityStatus: user.disabilityStatus,
+//                 jobLevel: user.jobLevel,
+//                 department: user.department,
+//                 companyId: user.companyId,
+//                 surveyId: user.surveyId, // Store surveyId
+//                 role: 'employee',
+//               });
+//         } catch (error) {
+//           throw new Error(`${error}`);
+//         }
 
-    }
+//     }
     
-  return `${employeeArray.length} employees created`
-};
+//   return `${employeeArray.length} employees created`
+// };
 
-export function colonStringToObject(input: string): {
-    firstName: string,
-    lastName: string,
-    email: string,
-    dob: string,
-    hireDate: string,
-    gender: string,
-    ethnicity: string,
-    manager: string,
-    location: string,
-    veteranStatus: string,
-    disabilityStatus: string,
-    jobLevel: string,
-    department: string,
-    companyId: string,
-    surveyId: string,
-    role: string
-  } | null {
+// export function colonStringToObject(input: string): {
+//     firstName: string,
+//     lastName: string,
+//     email: string,
+//     dob: string,
+//     hireDate: string,
+//     gender: string,
+//     ethnicity: string,
+//     manager: string,
+//     location: string,
+//     veteranStatus: string,
+//     disabilityStatus: string,
+//     jobLevel: string,
+//     department: string,
+//     companyId: string,
+//     surveyId: string,
+//     role: string
+//   } | null {
   
-    // Split the input string by the colon
-    const parts = input.split(':');
+//     // Split the input string by the colon
+//     const parts = input.split(':');
     
-    // Check if the input has the right number of parts (15 for fields + 1 for 'employee')
-    if (parts.length !== 16 || parts[15] !== 'employee') {
-      throw new Error(`Invalid format: ${input}`);
-    }
+//     // Check if the input has the right number of parts (15 for fields + 1 for 'employee')
+//     if (parts.length !== 16 || parts[15] !== 'employee') {
+//       throw new Error(`Invalid format: ${input}`);
+//     }
   
-    const [
-      firstName,
-      lastName,
-      email = "",
-      dob = "",
-      hireDate = "",
-      gender = "",
-      ethnicity = "",
-      manager = "",
-      location = "",
-      veteranStatus = "",
-      disabilityStatus = "",
-      jobLevel = "",
-      department = "",
-      companyId = "",
-      surveyId = ""
-    ] = parts.map(part => part.trim());
+//     const [
+//       firstName,
+//       lastName,
+//       email = "",
+//       dob = "",
+//       hireDate = "",
+//       gender = "",
+//       ethnicity = "",
+//       manager = "",
+//       location = "",
+//       veteranStatus = "",
+//       disabilityStatus = "",
+//       jobLevel = "",
+//       department = "",
+//       companyId = "",
+//       surveyId = ""
+//     ] = parts.map(part => part.trim());
   
-    // Throw error if firstName or lastName are missing
-    if (!firstName || !lastName) {
-      throw new Error(`First name and last name are required fields. Invalid input: ${input}`);
-    }
+//     // Throw error if firstName or lastName are missing
+//     if (!firstName || !lastName) {
+//       throw new Error(`First name and last name are required fields. Invalid input: ${input}`);
+//     }
   
-    // Return the object using the captured values
-    return {
-      firstName,
-      lastName,
-      email,
-      dob,
-      hireDate,
-      gender,
-      ethnicity,
-      manager,
-      location,
-      veteranStatus,
-      disabilityStatus,
-      jobLevel,
-      department,
-      companyId,
-      surveyId,
-      role: 'employee' // Hardcoded role
-    };
-}
+//     // Return the object using the captured values
+//     return {
+//       firstName,
+//       lastName,
+//       email,
+//       dob,
+//       hireDate,
+//       gender,
+//       ethnicity,
+//       manager,
+//       location,
+//       veteranStatus,
+//       disabilityStatus,
+//       jobLevel,
+//       department,
+//       companyId,
+//       surveyId,
+//       role: 'employee' // Hardcoded role
+//     };
+// }
 
   
   
--- amplify/data/snippets-handler/snippetsHandler.ts
+++ amplify/data/snippets-handler/snippetsHandler.ts
@@ -1,67 +1,67 @@
-import type { Schema } from '../resource'
-import { generateClient } from "aws-amplify/data";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+// import type { Schema } from '../resource'
+// import { generateClient } from "aws-amplify/data";
+// import { Amplify } from "aws-amplify";
+// import outputs from "@/amplify_outputs.json";
 
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
-export const handler: Schema["bulkCreateSnippets"]["functionHandler"] = async (event, context) => {
+// export const handler: Schema["bulkCreateSnippets"]["functionHandler"] = async (event, context) => {
     
-    let snippetsArray = event.arguments.snippetsArray || [];
+//     let snippetsArray = event.arguments.snippetsArray || [];
 
-    for (let i = 0; i < snippetsArray.length; i++) {
-        const snippetObject: any = parseTextToObj(snippetsArray[i] ? String(snippetsArray[i]) : '')
+//     for (let i = 0; i < snippetsArray.length; i++) {
+//         const snippetObject: any = parseTextToObj(snippetsArray[i] ? String(snippetsArray[i]) : '')
         
-        try {
-            await client.models.TextSnippet.create({
-              factor:  snippetObject.factor,
-              score: snippetObject.score,
-              snippetText: snippetObject.snippetText,
-              type: snippetObject.sanitizedType,
-              disabled: false,
-              snippetSetId: "",
-            });
-        } catch (error) {
-          throw new Error(`${error}`);
-        }
-    }
+//         try {
+//             await client.models.TextSnippet.create({
+//               factor:  snippetObject.factor,
+//               score: snippetObject.score,
+//               snippetText: snippetObject.snippetText,
+//               type: snippetObject.sanitizedType,
+//               disabled: false,
+//               snippetSetId: "",
+//             });
+//         } catch (error) {
+//           throw new Error(`${error}`);
+//         }
+//     }
     
-  return `${snippetsArray.length} snippets created`
-};
+//   return `${snippetsArray.length} snippets created`
+// };
 
-export function parseTextToObj(input: string): { factor: string, score: number, snippetText: string, sanitizedType: string } | null {
-    // Regular expression to match the structure: factor, score, snippetText, sanitizedType
-    const regex = /^([^:]+):(\d+):([^:]+):([^:]+)$/;
+// export function parseTextToObj(input: string): { factor: string, score: number, snippetText: string, sanitizedType: string } | null {
+//     // Regular expression to match the structure: factor, score, snippetText, sanitizedType
+//     const regex = /^([^:]+):(\d+):([^:]+):([^:]+)$/;
 
-    // Execute the regex to capture the parts of the string
-    const match = input.match(regex);
+//     // Execute the regex to capture the parts of the string
+//     const match = input.match(regex);
 
-    if (match && match.length === 5) {
-        const factor = match[1].trim();
-        const score = Number(match[2]);
-        const snippetText = match[3].trim();
-        const sanitizedType = match[4].trim();
+//     if (match && match.length === 5) {
+//         const factor = match[1].trim();
+//         const score = Number(match[2]);
+//         const snippetText = match[3].trim();
+//         const sanitizedType = match[4].trim();
 
-        // Array of valid sanitized types
-        const validTypes = ["adminoverview", "employeeaggregated", "employeeindividual"];
+//         // Array of valid sanitized types
+//         const validTypes = ["adminoverview", "employeeaggregated", "employeeindividual"];
 
-        // Validate the sanitizedType
-        if (!validTypes.includes(sanitizedType)) {
-            console.error(`Invalid type: ${sanitizedType}`);
-            return null;  // Return null if the type is invalid
-        }
+//         // Validate the sanitizedType
+//         if (!validTypes.includes(sanitizedType)) {
+//             console.error(`Invalid type: ${sanitizedType}`);
+//             return null;  // Return null if the type is invalid
+//         }
 
-        // Return the object if all checks pass
-        return {
-            factor,
-            score,
-            snippetText,
-            sanitizedType
-        };
-    } else {
-        throw new Error(`Invalid format: ${input}`);  // Throw error if input doesn't match
-    }
-}
+//         // Return the object if all checks pass
+//         return {
+//             factor,
+//             score,
+//             snippetText,
+//             sanitizedType
+//         };
+//     } else {
+//         throw new Error(`Invalid format: ${input}`);  // Throw error if input doesn't match
+//     }
+// }
